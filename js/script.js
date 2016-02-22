28ae8c3cbd22e33cfe8e856445b8b9facbfa7590
Joseph Anda <josephanda@Josephs-MacBook-Pro.local>
2016-02-22 10:55:55 -0800

    successful refactor

--- js/script.js
+++ js/script.js
@@ -1,46 +1,55 @@
 (function() {
 	"use strict";
-	
-	var menuIcon = document.getElementById('menu-icon'),
-		menuItems = document.querySelectorAll(".navbar > li"),
-	    links = document.querySelectorAll(".navbar > li > a"),
-	    opened = false;
-	
-	//  Wire up first menu icon
-	menuIcon.addEventListener('click', function() {
-		open();
-	});
 
-	//  Intended to fix an autoselect bug from rapid clicking
-	function deselectItemsIn(array) {
-		$.each(array, function( index, item) {
-			item.selected = false;
-		})
-	}
+	var model = { };
 
-	//  Boilerplate UI code for animating menu items 
-	function open() {
-		var slideDistance = 2;  //  control how far the menu item slides 
-		if (!opened) {
-			//  Move menu items down a relative amount 
-			for (var i = 0; i < menuItems.length; i++) {
-				menuItems[i].style.transition = 'all 0.2s ease';
-				menuItems[i].style.transform = 'translatey(' + slideDistance + 'em)';
-				slideDistance += 2;
-				menuItems[i].style.visibility = 'visible';
+	var view = {
+		init: function() {
+			this.menuIcon = document.getElementById('menu-icon');
+			this.menuItems = document.querySelectorAll(".navbar > li");
+			this.links = document.querySelectorAll(".navbar > li > a");
+			this.opened = false;
 				
-			}
-			opened = true;
+		},
+		openMenu: function() {
+			var slideDistance = 2;  //  control how far the menu item slides 
+			if (!this.opened) {
+				//  Move menu items down a relative amount 
+				for (var i = 0; i < this.menuItems.length; i++) {
+					this.menuItems[i].style.transition = 'all 0.2s ease';
+					this.menuItems[i].style.transform = 'translatey(' + slideDistance + 'em)';
+					slideDistance += 2;
+					this.menuItems[i].style.visibility = 'visible';
+				}
+				this.opened = true;
 
-		} else {
-			//  Move the menu items back up a relative amount 
-			for (var i = menuItems.length - 1; i >= 0; i--) {
-				menuItems[i].style.transition = 'none';
-				menuItems[i].style.visibility = 'hidden';
-				menuItems[i].style.transform = 'translatey(-' + slideDistance/2 + 'em)';
-				slideDistance -= 2;
+			} else {
+				//  Move the menu items back up a relative amount 
+				for (var i = this.menuItems.length - 1; i >= 0; i--) {
+					this.menuItems[i].style.transition = 'none';
+					this.menuItems[i].style.visibility = 'hidden';
+					this.menuItems[i].style.transform = 'translatey(-' + slideDistance/2 + 'em)';
+					slideDistance -= 2;
+				}
+				this.opened = false;
 			}
-			opened = false;
 		}
-	}
+	};
+
+	var controller = {
+		init: function() {
+			view.init();
+			view.menuIcon.addEventListener('click', function() {
+				view.openMenu();
+			});
+		},
+
+		deselectItemsIn: function(array) {
+			$.each(array, function( index, item) {
+				item.selected = false;
+			})
+		}
+	} 
+
+	controller.init();    	
 })();
